07cf27b1de43c489eb9c9ae2288408b84b396acf
Augusto Luba Birsnek <augustobirsnek@gmail.com>
2023-12-03 15:54:52 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "photo":"https://legadodamarvel.com.br/wp-content/uploads/2023/12/Vazamento-indica-cena-epica-de-Deadpool-3-que-pode-superar-Vingadores-Ultimato-legadodamarvel.webp",
+    "title":"Vazamento indica cena épica de Deadpool 3 que pode superar Vingadores: Ultimato",
+    "description":"Deadpool 3 deverá apresentar uma cena tão épica que pode acabar superando o grande momento de Vingadores: Ultimato."
+  },
+  {
+    "id":"2",
+    "photo":"https://legadodamarvel.com.br/wp-content/uploads/2023/12/Poder-secreto-do-Hulk-o-transformara-em-um-pesadelo-para-os-X-Men-no-MCU-legadodamarvel.webp",
+    "title":"Poder secreto do Hulk o transformará em um pesadelo para os X-Men no MCU",
+    "description":"Os X-Men estão chegando ao MCU e irão se deparar com o Hulk, que possui um superpoder que o transforma em um pesadelo para os mutantes."
+  },
+  {
+    "id":"3",
+    "photo":"https://legadodamarvel.com.br/wp-content/uploads/2023/12/Os-3-maiores-herois-da-Marvel-se-reunem-em-poster-epico-de-Vingadores-6-legadodamarvel.webp",
+    "title":"Os 3 maiores heróis da Marvel se reúnem em pôster épico de Vingadores 6",
+    "description":"Pôster feito para Vingadores: Guerras Secretas traz encontro épico entre o Homem de Ferro, Wolverine e Homem-Aranha - confira. A Marvel Studios continua avançando com a produção de dois novos filmes dos Vingadores, a equipe de heróis mais famosa do cinema, e que estarão de volta em 2026 e em 2027."
+  },
+  {
+    "id":"4",
+    "photo":"https://legadodamarvel.com.br/wp-content/uploads/2023/11/Marvel-anuncia-nova-serie-especial-de-Loki-que-estreia-no-Disney-ainda-neste-mes-legadodamarvel.webp",
+    "title":"Loki: Revelada exigência de Kevin Feige pro final da 2ª temporada",
+    "description":"A 2ª temporada de Loki chegou ao fim no mês passado. Confira como Kevin Feige interferiu no finale da série. A segunda temporada de Loki foi uma das melhores coisas feitas pela Marvel em muitos anos. Possivelmente dando um fim à própria série, tivemos seu último episódio exibido há algumas semanas no Disney+.E agora sabemos de um detalhe bem interessante sobre o último episódio, já que uma mudança foi feita no finalzinho por causa de Kevin Feige, o próprio Zé Boné! Em uma entrevista, a compositora Natalie Holt confirmou que a música final do episódio teria LETRAS. Porém, o Zé Boné insistiu para que ela ficasse apenas no instrumental."
+  },
+]
